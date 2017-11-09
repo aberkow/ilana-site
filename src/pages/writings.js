2607520126d56314a7e9f239bdb094057f609bdf
aberkow <berkowitz.clarinet@gmail.com>
2017-11-08 22:03:22 -0500

    article links

--- src/pages/writings.js
+++ src/pages/writings.js
@@ -7,6 +7,9 @@ const Writings = () =>
     <ul>
       <li><a href="https://www.huffingtonpost.com/rabbi-ilana-garber/singing-in-the-car_b_5682661.html">Singing in the Car</a></li>
       <li><a href="https://www.huffingtonpost.com/rabbi-ilana-garber/rabbis-have-feelings-too_b_5536369.html">Rabbis Have Feelings Too</a></li>
+      <li>
+        <a href="https://www.huffingtonpost.com/author/rabbi-ilana-garber">Read More Articles</a>
+      </li>
     </ul>
     <h2>The Jewish Week</h2>
     <ul>
@@ -15,6 +18,30 @@ const Writings = () =>
       </li>
       <li>
         <a href="http://jewishweek.timesofisrael.com/fragile-x-awareness-a-parent-speaks-out/">Fragile X Awareness: A Parent Speaks Out</a>
+      </li>
+    </ul>
+    <h2>Kveller</h2>
+    <ul>
+      <li>
+        <a href="https://www.kveller.com/what-a-candle-spice-box-and-grape-juice-can-really-do-for-your-family/">What a Candle, Spice Box, and Grape Juice Can Really Do for Your Family</a>
+      </li>
+      <li>
+        <a href="https://www.kveller.com/how-headphones-literally-saved-my-son-with-fragile-x-during-a-family-outing/">How Headphones Literally Saved My Son with Fragile X During a Family Outing</a>
+      </li>
+      <li>
+        <a href="https://www.kveller.com/why-ill-send-my-son-with-fragile-x-to-school-on-sukkot/">Why I’ll Send My Son with Fragile X to School on Sukkot</a>
+      </li>
+      <li>
+        <a href="https://www.kveller.com/so-what-exactly-is-tisha-bav/">So, What Exactly Is Tisha B’Av?</a>
+      </li>
+      <li>
+        <a href="https://www.kveller.com/my-very-jewish-moment-at-the-guns-n-roses-concert/">What Happens When a Rabbi Goes to a Guns N’ Roses Concert</a>
+      </li>
+      <li>
+        <a href="https://www.kveller.com/5-ways-to-raise-a-jew-by-choice/">5 Ways to Raise a Jew-By-Choice</a>
+      </li>
+      <li>
+        <a href="https://www.kveller.com/author/ilana-garber/">Read More Articles</a>
       </li>
     </ul>
     <h2>Others</h2>
