34a30498ec7183fcdf25d7834b1fe26da439167a
aberkow <berkowitz.clarinet@gmail.com>
2018-04-08 14:19:14 -0400

    bio

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Welcome!</h1>
     <div>
       <p>
-        Rabbi Ilana C. Garber has served the community of Beth El Temple since August 2005. She graduated from the Jewish Theological Seminary in New York, with a bachelor’s degree in Talmud (2000), a master’s degree in Jewish education (2003), and rabbinic ordination (2005). She also received a bachelor’s degree in religion from Barnard College (2000), writing a thesis entitled, “Can One be an Observant Jew and a Practicing Buddhist?” (You'll have to chat with her to find the answer!)
+        Since August 2005, I've served the community of <a href="https://www.bethelwesthartford.org/">Beth El Temple in West Hartford Connecticut</a>. I graduated from the Jewish Theological Seminary in New York, with a bachelor’s degree in Talmud (2000), a master’s degree in Jewish education (2003), and rabbinic ordination (2005). I have also received a bachelor’s degree in religion from Barnard College (2000), writing a thesis entitled, “Can One be an Observant Jew and a Practicing Buddhist?” (<Link to="/contact">You'll have to chat with her to find the answer!</Link>)
       </p>
     </div>
   </div>
